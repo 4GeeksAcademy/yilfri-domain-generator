2ac24083f2d287b8a570a789b60ca43e18a6c491
Yilfri Salave <yilfri97@hotmail.com>
2025-06-24 01:09:28 +0000

    add onLoad

--- src/app.js
+++ src/app.js
@@ -7,12 +7,15 @@ let pronoun = ['the', 'our'];
 let adj = ['great', 'big'];
 let noun = ['jogger', 'racoon'];
 
-
-for (let i = 0; i < pronoun.length; i++) {
-  for (let j = 0; j < adj.length; j++){
-    for (let k = 0; k < noun.length; k++){
-      console.log(pronoun[j] + adj[i] + noun[k] + ".com")
+window.onload = function () {
+  //write your code here
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        console.log(pronoun[j] + adj[i] + noun[k] + ".com")
+      }
     }
-  }   
-}
+  }
+};
+
 
